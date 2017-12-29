c8b65d85715339b59c06640d18aa3f9943683271
ehu <fehuehuf@gmail.com>
2017-12-29 21:25:55 +0900

    加速度センサーの実装

--- app/src/main/java/com/example/ehu/accball/MainActivity.java
+++ app/src/main/java/com/example/ehu/accball/MainActivity.java
@@ -1,13 +1,55 @@
 package com.example.ehu.accball;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    SensorManager mSensorManager;
+    Sensor mAccSensor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Androidに搭載されているサービスのマネージャークラスの生成
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        //サービスの中から、加速度センサーを選ぶ
+        mAccSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //加速度センサーの開始
+        mSensorManager.registerListener(this, mAccSensor, SensorManager.SENSOR_DELAY_GAME);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mSensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            Log.d("MainActivity",
+                    "x=" + String.valueOf(event.values[0]) +
+                            "y=" + String.valueOf(event.values[1]) +
+                            "z=" + String.valueOf(event.values[2]));
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
     }
 }
