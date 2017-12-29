41655be886e4978deafa3714f2c1171a82fea637
ehu <fehuehuf@gmail.com>
2017-12-29 23:51:29 +0900

    surfaceのコールバックを実装

--- app/src/main/java/com/example/ehu/accball/MainActivity.java
+++ app/src/main/java/com/example/ehu/accball/MainActivity.java
@@ -8,11 +8,19 @@ import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Surface;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity implements SensorEventListener {
+public class MainActivity extends AppCompatActivity implements SensorEventListener, SurfaceHolder.Callback {
 
+    public final String TAG = getClass().getSimpleName();
     SensorManager mSensorManager;
     Sensor mAccSensor;
+    SurfaceHolder mHolder;
+    int mSurfaceWidth;  //サーフェスビューの幅
+    int mSurfaceHeight; //サーフェスビューの高さ
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,25 +31,16 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         //サービスの中から、加速度センサーを選ぶ
         mAccSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        SurfaceView surfaceView = findViewById(R.id.surfaceView);
+        mHolder = surfaceView.getHolder();
+        mHolder.addCallback(this);
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        //加速度センサーの開始
-        mSensorManager.registerListener(this, mAccSensor, SensorManager.SENSOR_DELAY_GAME);
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        mSensorManager.unregisterListener(this);
-    }
 
     @Override
     public void onSensorChanged(SensorEvent event) {
         if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
-            Log.d("MainActivity",
+            Log.d(TAG,
                     "x=" + String.valueOf(event.values[0]) +
                             "y=" + String.valueOf(event.values[1]) +
                             "z=" + String.valueOf(event.values[2]));
@@ -51,5 +50,23 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     @Override
     public void onAccuracyChanged(Sensor sensor, int i) {
 
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder surfaceHolder) {
+        //加速度センサーの開始
+        mSensorManager.registerListener(this, mAccSensor, SensorManager.SENSOR_DELAY_GAME);
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+        mSurfaceWidth = width;
+        mSurfaceHeight = height;
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+        //加速度センサーの停止
+        mSensorManager.unregisterListener(this);
     }
 }
